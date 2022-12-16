881be54877767f7c2dd7008247b8c21e33523555
Matrix <rhailu88@gmail.com>
2022-12-16 13:47:05 -0500

    Created employees table in schema.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,16 @@ CREATE TABLE role (
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
