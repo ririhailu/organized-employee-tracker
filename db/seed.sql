d53f9b067c15842b6d3c4b829637dc2d37c7bca6
Matrix <rhailu88@gmail.com>
2022-12-16 13:50:58 -0500

    Added insert into for employee data in seed.sql.

--- db/seed.sql
+++ db/seed.sql
@@ -19,3 +19,16 @@ INSERT INTO role (title, salary, department_id) VALUES
 ("Engineer", 100000, 4),
 ("HR Manager", 80000, 5),
 ("HR Associate", 60000, 5);
+
+-- Employees Data
+INSERT INTO employees (first_name, last_name, role_id, manager_id, department_id) VALUES
+("Kouassi", "Kouadio", 7, NULL, 4),
+("Jake", "Albano", 5, NULL, 3),
+("Mick", "Jason", 8, 1, 4),
+("Mary", "Jane", 1, NULL, 1),
+("Richard", "Cook", 3, NULL, 2),
+("Jean", "Walker", 6, 2, 3),
+("Sonia", "Twains", 2, 4, 1),
+("Sylvia", "Kim", 4, 5, 2),
+("Marc", "Rich", 9, NULL, 5),
+("Bryan", "Ken", 10, 9, 5),
