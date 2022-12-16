40aa72e69671b8858fbd9c59a9556cb28cb90ec1
Matrix <rhailu88@gmail.com>
2022-12-16 13:57:41 -0500

    Added function that starts the app and questions in employeeTracker.js file.

--- employeeTracker.js
+++ employeeTracker.js
@@ -4,3 +4,26 @@ const mysql2 = require("mysql2")
 const consoleTable = require("console.table");
 
 const connection = require('./config/connection')
+
+
+//Function that starts the app and prompt the questions
+function startApp() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View departments",
+        "View roles",
+        "Add department",
+        "Add role",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "EXIT"
+      ]
+    })
