b985b85efb1cc13e6c9295711c339e08a7901aa0
Matrix <rhailu88@gmail.com>
2022-12-16 13:49:48 -0500

    Added insert into for role data in seed.sql.

--- db/seed.sql
+++ db/seed.sql
@@ -6,3 +6,16 @@ INSERT INTO departments (name) VALUES
 ("Operations"),
 ("Engineering"),
 ("Human-ressouces");
+
+-- Roles Data
+INSERT INTO role (title, salary, department_id) VALUES 
+("Accountant Manager", 100000, 1),
+("Financial Associate", 62000, 1),
+("Sales Manager", 90000, 2),
+("Sales Associate", 50000, 2),
+("Operations Manager", 80000, 3),
+("Operations Associate", 50000, 3),
+("Chef Engineer", 110000, 4),
+("Engineer", 100000, 4),
+("HR Manager", 80000, 5),
+("HR Associate", 60000, 5);
