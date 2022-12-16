806805a52b35357d92313d1c9cdd8faf047ba940
Matrix <rhailu88@gmail.com>
2022-12-16 13:44:25 -0500

    Created role table in schema.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE departments (
   name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
