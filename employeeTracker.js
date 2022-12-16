a1cc265f8340638c690e26250b3cb7c0a8f37a65
Matrix <rhailu88@gmail.com>
2022-12-16 14:18:44 -0500

    Added inquirer questions and function to remove employee in employeeTracker.js file.

--- employeeTracker.js
+++ employeeTracker.js
@@ -126,3 +126,162 @@ function addEmployee() {
       var roleList = resRole[i].title;
       roleChoice.push(roleList);
     };
+
+    var deptChoice = [];
+    connection.query("SELECT * FROM departments", function(err, resDept) {
+      if (err) throw err;
+      for (var i = 0; i < resDept.length; i++) {
+        var deptList = resDept[i].name;
+        deptChoice.push(deptList);
+    }
+
+    inquirer
+    .prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "Enter employee's first name:"
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "Enter employee's last name:"
+    },
+    {
+      name: "role_id",
+      type: "rawlist",
+      message: "Select employee role:",
+      choices: roleChoice
+    },
+    {
+      name: "department_id",
+      type: "rawlist",
+      message: "Select employee's department:",
+      choices: deptChoice
+    },
+
+  ])
+    .then(function(answer) {
+      //for loop to retun 
+      var chosenRole;
+        for (var i = 0; i < resRole.length; i++) {
+          if (resRole[i].title === answer.role_id) {
+            chosenRole = resRole[i];
+          }
+        };
+
+        var chosenDept;
+        for (var i = 0; i < resDept.length; i++) {
+          if (resDept[i].name === answer.department_id) {
+            chosenDept = resDept[i];
+          }
+        };
+      //connection to insert response into database  
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: chosenRole.id,
+          department_id: chosenDept.id
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Employee " + answer.firstName + " " + answer.lastName + " successfully added!");
+          startApp();
+        }
+      );
+    })
+   });
+  })
+};
+
+//Function to add department
+function addDept() {
+  inquirer
+    .prompt([
+    {
+      name: "dept",
+      type: "input",
+      message: "Enter new department's name:"
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO departments SET ?",
+      {
+        name: answer.dept
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Department " + answer.dept + " successfully added!");
+        startApp();
+      }
+    );
+  });
+};
+
+//Function to new add role
+function addRole() {
+  var deptChoice = [];
+    connection.query("SELECT * FROM departments", function(err, resDept) {
+      if (err) throw err;
+      for (var i = 0; i < resDept.length; i++) {
+        var deptList = resDept[i].name;
+        deptChoice.push(deptList);
+    }
+
+  inquirer
+  .prompt([
+  {
+    name: "title",
+    type: "input",
+    message: "Enter new role's name:"
+  },
+  {
+    name: "salary",
+    type: "number",
+    message: "Enter new role's salary:"
+  },
+  {
+    name: "department_id",
+    type: "rawlist",
+    message: "Select employee's department:",
+    choices: deptChoice
+  }
+])
+.then(function(answer) {
+
+  var chosenDept;
+        for (var i = 0; i < resDept.length; i++) {
+          if (resDept[i].name === answer.department_id) {
+            chosenDept = resDept[i];
+          }
+        };
+
+  connection.query(
+    "INSERT INTO role SET ?",
+    {
+      title: answer.title,
+      salary:answer.salary,
+      department_id: chosenDept.id
+    },
+    function(err) {
+      if (err) throw err;
+      console.log("New role " + answer.title + " successfully added!");
+      startApp();
+    }
+  );
+});
+})
+};
+
+//Function to remove employee
+function removeEmployee() {
+  var empChoice = [];
+    connection.query("SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees", function(err, resEmp) {
+      if (err) throw err;
+      for (var i = 0; i < resEmp.length; i++) {
+        var empList = resEmp[i].name;
+        empChoice.push(empList);
+    };
